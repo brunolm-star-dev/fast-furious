2336b0cb08c686d0a2e45d23b592af8e882965bd
Bruno <brunodevprogramador@gmail.com>
2024-03-08 13:40:53 -0300

    feat: fast furious

---
+++ scripts.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("dodge");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://flatout.com.br/os-segredos-do-dodge-charger-de-dom-toretto-no-primeiro-velozes-e-furiosos/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("sky");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://www.aquelamaquina.pt/noticias/actualidade/detalhe/velocidade-furiosa-os-segredos-do-nissan-skyline-de-bryan-oconner.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("eclipse");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://motor1.uol.com.br/news/560895/mitsubishi-eclipse-velozes-furiosos-leilao/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("dodge-challenger");
+    imagem.addEventListener("click", function () {
+        window.location.href = "http://www.memoriamotor.com.br/dodge-e-srt-brilham-em-velozes-furiosos-6/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("supra");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://autoesporte.globo.com/carros/noticia/2022/05/que-fim-levou-o-toyota-supra-do-braia-o-brian-oconner-de-velozes-e-furiosos.ghtml";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("s2000");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://www.4mengarage.com.br/post/honda-s2000-droga-%C3%A9-a-suki";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("rx7");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://autopapo.uol.com.br/curta/mazda-rx7-velozes-furiosos/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("mustang");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://jornaldocarro.estadao.com.br/carros/mustang-usado-em-velozes-e-furiosos-esta-venda/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("nissan");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://www.revistafullpower.com.br/350z-do-filme-velozes-e-furiosos-de-toquio-esta-venda-por-r-440-mil/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("Rx7");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://autopapo.uol.com.br/curta/mazda-rx-7-velozes-furiosos-leilao/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("lykan");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://motorshow.com.br/lykan-hypersport-de-velozes-e-furiosos-7-vai-a-leilao/";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var imagem = document.getElementById("civic");
+    imagem.addEventListener("click", function () {
+        window.location.href = "https://www.razaoautomovel.com/noticias/velocidade-furiosa-honda-civic-ej1-segredos/";
+    });
+});
+
+
+
+
+
